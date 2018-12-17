b9a9ee4881f2c46a99fb83b104f593d2a2d8ec35
yuriymorkovnik <yuriymork@yandex.ru>
2018-12-17 17:20:19 +0200

    added downshift

--- src/components/FilterBar/OriginFilter.js
+++ src/components/FilterBar/OriginFilter.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import PropTypes from 'prop-types';
+import Downshift from 'downshift';
 import { formValueSelector } from 'redux-form'
 import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
 
 import { connectAll } from "../../utils";
@@ -11,19 +13,43 @@ const styles = {
   root: {
     display: 'flex',
     flexDirection: 'column',
+    position: 'relative',
   },
   select: {
     maxWidth: '240px',
   },
+  container: {
+    position: 'relative',
+  },
+  suggestList: {
+    position: 'absolute',
+    // left: 0,
+    // right: 0,
+    zIndex: 10,
+  }
 };
 
 class OriginFilter extends Component {
   state = {
     origins: null,
+    menuIsOpen: false,
   };
-  handleChange = ({ target: { value } }) => {
+  handleInputChange = ({ target: { value } }) => {
+    this.setState({ menuIsOpen: Boolean(value) });
     this.props.input.onChange(value);
   };
+
+  handleChange = item => {
+    console.log('chpok');
+    this.setState({ menuIsOpen: false });
+    this.props.input.onChange(item);
+  };
+
+  onOuterClick = () => {
+    this.setState({menuIsOpen: false});
+  };
+
+
   static getDerivedStateFromProps(props) {
     if(props.origins) {
       return {
@@ -33,25 +59,58 @@ class OriginFilter extends Component {
     return null;
   }
 
+  getSuggestions = (str) => {
+    return this.props.origins.filter(origin => origin.toLowerCase().includes(str.toLowerCase()));
+  };
+
   render() {
     const {
       classes,
-      origins,
       input: { value }
     } = this.props;
     return (
-      <TextField
-        className={classes.root}
-        id="outlined-select-currency"
-        label="Origin"
-        value={value}
+      <Downshift
+        inputValue={value}
+        isOpen={this.state.menuIsOpen}
+        onOuterClick={this.onOuterClick}
         onChange={this.handleChange}
-        variant="outlined"
       >
-        {origins.map(item => (
-          <MenuItem key={item} value={item}>{item}</MenuItem>
-        ))}
-      </TextField>
+        {({
+            getInputProps,
+            getItemProps,
+            getLabelProps,
+            getMenuProps,
+            isOpen,
+            inputValue,
+          }) => (
+          <div className={classes.container}>
+            <TextField
+              {...getInputProps()}
+              className={classes.root}
+              id="outlined-select-currency"
+              label="Origin"
+              value={inputValue}
+              onChange={this.handleInputChange}
+              variant="outlined"
+            />
+            {
+              isOpen ? (
+                <Paper
+                  className={classes.suggestList}
+                  {...getMenuProps()}
+                  square
+                >
+                  {this.getSuggestions(inputValue).map((item, index) => (
+                    <MenuItem key={item} {...getItemProps({ item, index })}>{item}</MenuItem>
+                  ))}
+                </Paper>
+              ) : null
+            }
+          </div>
+        )}
+
+      </Downshift>
+
     )
   }
 }
